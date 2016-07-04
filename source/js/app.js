f80abf4ab4b74da57ddd8ff4bac35994dc2c15cc
evd1ser <evd1ser@gmail.com>
2016-07-04 16:02:55 +0300

    js relise

--- source/js/app.js
+++ source/js/app.js
@@ -123,11 +123,15 @@
     /*
     menu svg
      */
-    var pathA = document.getElementById('pathA'),
-        pathC = document.getElementById('pathC'),
-        segmentA = new Segment(pathA, 8, 32),
-        segmentC = new Segment(pathC, 8, 32);
-
+    if ($('*').is('#pathA')) {
+        var pathA = document.getElementById('pathA'),
+            pathB = document.getElementById('pathB'),
+            pathC = document.getElementById('pathC'),
+            segmentA = new Segment(pathA, 8, 32),
+            segmentB = new Segment(pathB, 8, 32),
+            segmentC = new Segment(pathC, 8, 32),
+            toCloseIcon = true;
+    }
 
     function inAC(s) {
         s.draw('80% - 24', '80%', 0.3, {delay: 0.1, callback: function(){
@@ -140,8 +144,6 @@
 
 
 // Initialize
-    var pathB = document.getElementById('pathB'),
-        segmentB = new Segment(pathB, 8, 32);
 
 // Expand the bar a bit
     function inB(s) { s.draw(8 - 6, 32 + 6, 0.1, {callback: function(){ inB2(s) }}); }
@@ -157,12 +159,11 @@
     function outB(s) { s.draw(8, 32, 0.7, {delay: 0.1, easing: ease.ease('elastic-out', 2, 0.4)}); }
 
 // Run the animations
-    var trigger = document.getElementById('menu-icon-trigger'),
-        toCloseIcon = true;
 
-    
-    
-    
+
+
+
+
     $('.js-menu').click(function() {
         if (toCloseIcon) {
             inAC(segmentA);
@@ -201,4 +202,125 @@
 
 
     });
+
+    $(".js-scrollto").click(function () {
+        var elementClick = $(this).attr("href");
+        var destination = $(elementClick).offset().top;
+        jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 800);
+        return false;
+    });
+
 })();
+
+
+
+    $('.js-submit').click(function (e) {
+        e.preventDefault();
+        var
+            $okey = 1,
+            $logingrup = $('#logingrup'),
+            $passwordgrup = $('#passwordgrup'),
+            $robots = $('#robots'),
+            $login = $('#login').val(),
+            $password = $('#password').val(),
+            $ishuman = $('#human'),
+            $humanrealy = $('#yes:radio:checked').prop("checked");
+
+
+        $logingrup.removeClass('b-formgrup_success b-formgrup_err');
+        $passwordgrup.removeClass('b-formgrup_success b-formgrup_err');
+        $ishuman.removeClass('b-portfolio__checkbox_success b-portfolio__checkbox_err');
+        $robots.removeClass('b-formgrup__text_success b-formgrup__text_err');
+
+        if (!$login){
+            $okey = $okey * 0;
+            $logingrup.addClass('b-formgrup_err');
+        } else {
+            $okey = $okey * 1;
+            $logingrup.addClass('b-formgrup_success');
+        }
+
+        if (!$password){
+            $okey = $okey * 0;
+            $passwordgrup.addClass('b-formgrup_err');
+        } else {
+            $okey = $okey * 1;
+            $passwordgrup.addClass('b-formgrup_success');
+        }
+
+        if (!$ishuman.prop("checked")){
+            $okey = $okey * 0;
+            $ishuman.addClass('b-portfolio__checkbox_err');
+        } else {
+            $okey = $okey * 1;
+            $ishuman.addClass('b-portfolio__checkbox_success');
+        }
+
+        if (!$humanrealy){
+            $okey = $okey * 0;
+            $robots.addClass('b-formgrup__text_err');
+        } else {
+            $okey = $okey * 1;
+            $robots.addClass('b-formgrup__text_success');
+        }
+
+        if ($okey) {
+            console.log('Юхуууу, выполняем аякс запрос на сервер!')
+        } else {
+            console.log('увы, err')
+        }
+
+    });
+
+
+
+    $('.js-submit_message').click(function (e) {
+        e.preventDefault();
+        var
+            $okey = 1,
+            $namegrup = $('#namegrup'),
+            $emailgrup = $('#emailgrup'),
+            $message = $('#message');
+
+
+        $namegrup.removeClass('b-formgrup_success b-formgrup_err');
+        $emailgrup.removeClass('b-formgrup_success b-formgrup_err');
+        $message.removeClass('b-form-a__textarea_success b-form-a__textarea_err');
+
+
+
+        if (!$namegrup.children('.b-form-a__input').val()){
+            $okey = $okey * 0;
+            $namegrup.addClass('b-formgrup_err');
+        } else {
+            $okey = $okey * 1;
+            $namegrup.addClass('b-formgrup_success');
+        }
+
+        if (!$emailgrup.children('.b-form-a__input').val()){
+            $okey = $okey * 0;
+            $emailgrup.addClass('b-formgrup_err');
+        } else {
+            $okey = $okey * 1;
+            $emailgrup.addClass('b-formgrup_success');
+        }
+
+        if (!$message.val()){
+            $okey = $okey * 0;
+            $message.addClass('b-form-a__textarea_err');
+        } else {
+            $okey = $okey * 1;
+            $message.addClass('b-form-a__textarea_success');
+        }
+
+
+
+        if ($okey) {
+            console.log('Юхуууу, выполняем аякс запрос на сервер!')
+        } else {
+            console.log('увы, err')
+        }
+        var $okey = 1;
+
+
+    });
